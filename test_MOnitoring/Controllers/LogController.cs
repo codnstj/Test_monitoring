ba1afd2fbbcbb8aa97e025fd36f7dc16ba764771
lot8220 <lot8229@kakao.com>
2021-09-14 13:59:16 +0900

    [UPATE] Adding LogController

---
+++ test_MOnitoring/Controllers/LogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using Serilog.Core;
+using Serilog.Formatting.Elasticsearch;
+namespace FluentdTester
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly Logger logger;
+        public LogController() {
+            logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Service", "LogTester")
+                .WriteTo.File(new ElasticsearchJsonFormatter(), "logs/fluentd-test.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+        // GET: api/log/test
+        [HttpGet("test")]
+        public IActionResult Get(){
+            logger.Debug(string.Format("This is sample log."));
+            return Ok();
+        }
+    }
+}
